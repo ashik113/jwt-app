5666895c3f37a90775aa7e58695cb7b1b1f63c1c
ashik113 <ashikcsbtech@gmail.com>
2024-12-18 23:01:06 +0530

    JWT Implementation version-1 done

---
+++ src/main/java/com/example/spring_security_jwt/security/MyUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.example.spring_security_jwt.security;
+
+import com.example.spring_security_jwt.entity.User;
+import com.example.spring_security_jwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Component
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> isAvailableUser = userRepository.findByEmail(email);
+        if(isAvailableUser.isEmpty()) {
+            throw new UsernameNotFoundException("Could not finduser with email = " + email);
+        }
+        User user = isAvailableUser.get();
+        return new org.springframework.security.core.userdetails.User(
+                email,
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
+}
